a2621a5066ee0fe578cb902e40620791fa596f4f
Zachary Cheshire <zcheshire3@gatech.edu>
2019-02-26 17:49:39 -0500

    Created project

---
+++ ride/ride/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ride
+//
+//  Created by Zachary Cheshire on 2/26/19.
+//  Copyright © 2019 zcheshire. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
